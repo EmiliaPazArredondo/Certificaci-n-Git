3fde4ec0d3b48a6f478ad17d5bccc0f5dba31f28
Emilia Paz <167714900+EmiliaPazArredondo@users.noreply.github.com>
2024-11-27 11:35:51 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,16 @@
+document.getElementById('downloadButton').onclick = function() {
+    alert('Thanks for downloading!');
+};
+
+document.getElementById('macLink').onclick = function() {
+    alert('Beginning download...');
+};
+
+document.getElementById('windowsLink').onclick = function() {
+    alert('Error 404 Not Found');
+};
+
+document.getElementById('linuxLink').onclick = function() {
+    alert('Error 404 Not Found');
+};
+
